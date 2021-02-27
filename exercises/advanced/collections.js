2b24c3f2d63d70087fbba6b221c8e639b2f254d3
Devon Poston <dev.poston@gmail.com>
2021-02-27 13:32:04 -0800

    working on promises adv content

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,11 +7,42 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
-
+const fs = require('fs');
+const Promise = require('bluebird');
+const FS = Promise.promisifyAll(require('fs'));
 
 var combineFirstLineOfManyFiles = function(filePaths, writePath) {
-  // TODO
+  let newArr = filePaths.map((filePath) => {
+    return FS.readFileAsync(filePath)
+      .then((data) => {
+        let firstLine = data.toString().split('\n')[0];
+        return firstLine;
+      })
+      .catch((error) => {
+        console.log('readFile failed', error);
+      });
+  });
+
+  Promise.all(newArr)
+    .then((newArr) => {
+      let joinArr = newArr.join('\n');
+      console.log('this is newarr:', joinArr);
+      return FS.writeFileAsync(writePath, joinArr)
+        .then((data) => {
+          console.log('this works', data);
+        })
+        .catch((error) => {
+          console.log('inside writefile', error);
+        });
+    })
+    .catch((error) => {
+      console.log('writeFile failed', error);
+    });
 };
+
+
+
+// let firstLine = response.toString().split('\n')[0];
 
 // Export these functions so we can unit test them
 module.exports = {
